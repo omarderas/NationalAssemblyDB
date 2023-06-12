eae6356e7724f9e715ea6633417282f473650584
omarderas <bz.inlandtours@gmail.com>
2023-06-11 23:46:32 -0600

    added deployment link

--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
