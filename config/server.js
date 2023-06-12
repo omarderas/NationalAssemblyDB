8e2fa50eb058d79bfd73f2796fc72e65d3f1304d
omarderas <bz.inlandtours@gmail.com>
2023-06-11 21:41:15 -0600

    database updates

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  settings: {
-    session: {
+ 
+  session: {
       keys: ['5fcMd2/nmTY6/3c4BoE', '9N38ir620Hjx+jTVeMr'],
     },
-  },
+  
   app: {
     keys: env.array('APP_KEYS'),
   },
