e6771eacce120b3ecee96e2f6d96d0b05d21be7a
omarderas <bz.inlandtours@gmail.com>
2023-06-11 22:58:59 -0600

    added deployment link

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,7 +4,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:1338','http://localhost:8000', 'https://dazzling-love-27c27bfaa6.strapiapp.com/'], 
+      origin: ['http://localhost:1338','http://localhost:8000', 'https://dazzling-love-27c27bfaa6.strapiapp.com/admin'], 
       headers: [
         'Content-Type',
         'Authorization',
