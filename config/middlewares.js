427151c9531aef06d888fabf5e5f5f847523e633
omarderas <bz.inlandtours@gmail.com>
2023-06-27 18:56:52 -0600

    CORS Policy updates

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,7 +4,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-     origin: ['http://localhost:1338','http://localhost:8000', 'https://dazzling-love-27c27bfaa6.strapiapp.com'],
+     origin: ['http://localhost:1338','http://localhost:8000', 'https://dazzling-love-27c27bfaa6.strapiapp.com','https://nationalassembly.netlify.app/'],
     // origin: ['http://localhost:1338','http://localhost:8000'],  
       headers: [
         'Content-Type',
