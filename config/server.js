f51be7fd5ecc3e4f9ec2641dd311c75b43c164ac
omarderas <bz.inlandtours@gmail.com>
2023-07-02 21:56:56 -0600

    User role field added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  url: 'https://dazzling-love-27c27bfaa6.strapiapp.com',
+ // url: 'https://dazzling-love-27c27bfaa6.strapiapp.com',
 
   app: {
     keys: env.array('APP_KEYS'),
