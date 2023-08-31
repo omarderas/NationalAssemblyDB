725a15a1f3844b46a00455ca317f08a935d37428
omarderas <bz.inlandtours@gmail.com>
2023-08-30 22:26:52 -0600

    Strapi cloud configs added

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,8 +5,8 @@ module.exports = [
     name: 'strapi::cors',
     config: {
    
-     //origin: ['http://localhost:1338','http://localhost:8000', 'https://dazzling-love-27c27bfaa6.strapiapp.com','https://nationalassembly.netlify.app'],
-    origin: ['http://localhost:1338','http://localhost:8000','http://127.0.0.1:1338','https://nationalassembly.netlify.app','https://app.netlify.com/'],  
+     origin: ['http://localhost:1338','http://localhost:8000', 'https://useful-splendor-947a2a8610.strapiapp.com','https://nationalassembly.netlify.app'],
+    //origin: ['http://localhost:1338','http://localhost:8000','http://127.0.0.1:1338','https://nationalassembly.netlify.app','https://app.netlify.com/'],  
       headers: [
         'Content-Type',
         'Authorization',
