753434298eddde54d38c26bd6985dbcd880e721a
omarderas <bz.inlandtours@gmail.com>
2023-09-05 23:07:16 -0600

    Strapi cloud configs added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
- // url: 'https://peaceful-courage-95a6513f8d.strapiapp.com/',
+  url: 'https://peaceful-courage-95a6513f8d.strapiapp.com',
 
   app: {
     keys: env.array('APP_KEYS'),
