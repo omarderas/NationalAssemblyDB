c67cb85702715e516b39e5bce341fb26cbc1fa0e
omarderas <bz.inlandtours@gmail.com>
2023-09-26 13:08:29 -0600

    server file updates

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
    
-     origin: ['http://localhost:1338','http://localhost:8000', 'https://peaceful-courage-95a6513f8d.strapiapp.com','https://nationalassembly.netlify.app','https://app.netlify.com/'],
+     origin: ['http://localhost:1338','http://localhost:8000', 'https://healing-acoustics-da118aead2.strapiapp.com','https://nationalassembly.netlify.app','https://app.netlify.com/'],
     //origin: ['http://localhost:1338','http://localhost:8000','http://127.0.0.1:1338','https://nationalassembly.netlify.app','https://app.netlify.com/'],  
       headers: [
         'Content-Type',
